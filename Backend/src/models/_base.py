65b3380459b8efc38ee6bdad756442e6566c3219
Valentin Haas <62573455+Energeneer@users.noreply.github.com>
2024-11-30 00:46:07 +0100

    WIP: Begin moving to sqlmodel with base class and requirements.txt

--- Backend/src/models/_base.py
+++ Backend/src/models/_base.py
@@ -3,33 +3,15 @@
 
 # Author: Valentin Haas, 2024
 
-# System imports
-from typing import Type, TypeVar
-
 # Library imports
-from pydantic import BaseModel
-from sqlalchemy.orm import DeclarativeBase
-
-# Constants
-T = TypeVar("T", bound=BaseModel)
-
-
-# declarative base class
-class BaseTable(DeclarativeBase):
-    """Base class for all SQLAlchemy database models, providing common functionality."""
-
+from sqlmodel import SQLModel
+from pydantic import ConfigDict
+    
+class BaseModel(SQLModel):
+    """Base class for all models, providing common functionality and configuration."""
+    
+    # Do not generate a table for the base model
     __abstract__ = True
 
-    @staticmethod
-    def to_pydantic(db_object: "BaseTable", pydantic_model: Type[T]) -> T:
-        """
-        Convert a database object to a Pydantic model.
-
-        Args:
-            db_object (BaseTable): The database object to convert.
-            pydantic_model (Type[T]): The Pydantic model to convert to.
-
-        Returns:
-            T: The Pydantic model, filled with the data from the database object.
-        """
-        return pydantic_model(**db_object.__dict__)
+    # Generak pydantic configuration for all models
+    model_config = ConfigDict(populate_by_name=True)

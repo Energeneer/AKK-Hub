20bbc4b7b2b47379923215468ca0c1f29d09b7ad
Valentin Haas <76203519+ValeHaas@users.noreply.github.com>
2025-01-06 20:11:12 +0100

    FIX: Reference to composite roominfluence primary key

--- Backend/src/models/roominflucenceupdates.go
+++ Backend/src/models/roominflucenceupdates.go
@@ -10,12 +10,13 @@ import (
 
 // RoomInflucenceUpdates is a model to track the updates of RoomInfluences.
 type RoomInflucenceUpdate struct {
-	gorm.Model                    // Provides ID, CreatedAt, UpdatedAt, DeletedAt fields
-	RoomInfluence   RoomInfluence // Referenced RoomInfluence Object
-	RoomInfluenceID uint          `gorm:"not null"` // The RoomInfluence that was updated (foreign key reference to RoomInfluences.Id).
-	Type            UpdateType    `gorm:"not null"` // The type of the update.
-	Title           string        `gorm:"not null"` // The title of the update.
-	UpdatedBy       User          // Referenced User Object
-	UpdatedByID     uint          `gorm:"not null"`     // The user who updated the group (foreign key reference to Users.Id).
-	Text            string        `gorm:"default:null"` // The text of the update.
+	gorm.Model                                // Provides ID, CreatedAt, UpdatedAt, DeletedAt fields
+	RoomInfluence               RoomInfluence // Referenced RoomInfluence Object
+	RoomInfluenceOccupiedRoomID uint          `gorm:"not null"` // The RoomInfluence that was updated (foreign key reference to RoomInfluences.OccupiedRoomID).
+	RoomInfluenceAffectedRoomID uint          `gorm:"not null"` // The RoomInfluence that was updated (foreign key reference to RoomInfluences.AffectedRoomID).
+	Type                        UpdateType    `gorm:"not null"` // The type of the update.
+	Title                       string        `gorm:"not null"` // The title of the update.
+	UpdatedBy                   User          // Referenced User Object
+	UpdatedByID                 uint          `gorm:"not null"`     // The user who updated the group (foreign key reference to Users.Id).
+	Text                        string        `gorm:"default:null"` // The text of the update.
 }
